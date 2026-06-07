787a43193002ee898ced5eef1b75559f3bd49f8e
Sudhir Krisna <sudhboiii@gmail.com>
2026-06-07 00:24:36 -0300

    updated version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-path = str(Path('../..', 'src').resolve())
+path = str(Path("../..", "src").resolve())
 print(path)
 sys.path.insert(0, path)
 # Configuration file for the Sphinx documentation builder.
@@ -12,32 +12,31 @@ sys.path.insert(0, path)
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'free_groups_26'
-copyright = '2026, Sudhir Krisna'
-author = 'Sudhir Krisna'
-release = 'v1.0.1'
+project = "free_groups_26"
+copyright = "2026, Sudhir Krisna"
+author = "Sudhir Krisna"
+release = "v1.1.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autosummary',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autosummary",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_logo = '_static/logo.svg'
-html_favicon = '_static/logo.svg'
-html_static_path = ['_static']
+html_theme = "furo"
+html_logo = "_static/logo.svg"
+html_favicon = "_static/logo.svg"
+html_static_path = ["_static"]
 add_module_names = False
-autoclass_content = 'both'
-autodoc_member_order = 'bysource'
+autoclass_content = "both"
+autodoc_member_order = "bysource"
